c5212e3559037c2df48ab945f48dc5a0f86e9333
Ray Sun <xiaoquqi@gmail.com>
2025-11-19 06:16:57 +0800

    Build only for tags push and add a empty line in models.py

--- devify/accounts/models.py
+++ devify/accounts/models.py
@@ -26,6 +26,7 @@ Other methods to extend the User model:
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Profile(models.Model):
     """
     The Profile model extends the built-in User model using a one-to-one
